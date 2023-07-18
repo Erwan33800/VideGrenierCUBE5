ecbc9e56ae8ed48ac42b396d0e1a26e9733e8100
Erwan LE PIERRES <55241151+Erwan33800@users.noreply.github.com>
2023-07-18 23:17:32 +0200

    script works

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,18 @@ clone_branch() {
   source_branch=$1
   target_branch=$2
 
+  echo "Clonage de la branche $source_branch vers la branche $target_branch..."
+
   git checkout $source_branch
   git pull origin $source_branch
   git checkout -b $target_branch
-  git push origin $target_branch
+  git push --set-upstream origin $target_branch
+
+  echo "Le clonage de la branche $source_branch vers la branche $target_branch est terminé."
 }
 
 # Cloner et monter depuis develop vers preprod
-clone_branch develop preprod
+clone_branch test-staging test-preprod10
 
 # Cloner et monter depuis preprod vers master
-clone_branch preprod master
+clone_branch test-preprod10 test-master10
